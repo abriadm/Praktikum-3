c4e9fb91904f78b3714a4a3498aee862c61767e0
Giveonaldo <giveonaldo@gmail.com>
2023-03-18 13:03:21 +0700

    Praktikum-3

---
+++ script.js
@@ -0,0 +1,12 @@
+Date()
+
+let tanggal = new Date()
+document.getElementById("tanggal").innerHTML = tanggal
+
+function tampil(){
+    alert("Ini adalah button")
+}
+
+function show(){
+    alert("Ini adalah po up untuk tulisan perkenalan")
+}
